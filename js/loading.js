6ed0e3f850560ff2985bff56b49a65b97360a2f6
chanchi- <chanchi0707@gmail.com>
2016-10-17 17:46:58 +0900

    ローディング画面の実装

---
+++ js/loading.js
@@ -0,0 +1,15 @@
+// $(function() {
+//   var h = $(window).height();
+ 
+//   $('.wrapper').css('display','none');
+//   $('.loader,.load').height(h).css('display','block');
+// });
+$(function(){
+
+	setTimeout(function(){
+		$('.wrapper').css('display','block');
+		$('.loader').delay(900).fadeOut(800);
+		$('.load').delay(600).fadeOut(300);
+	},5000);
+ 
+});
